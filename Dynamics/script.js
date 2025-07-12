df82120cac7389e970f7b7906785183bebf8798e
ShayanSheikh1936 <sheikhshayan082@gmail.com>
2025-07-12 18:21:01 +0500

    latest Version

--- Dynamics/script.js
+++ Dynamics/script.js
@@ -1,35 +1,95 @@
+function revealOnScroll() {
+  const cards = document.querySelectorAll('.work-cards, .work-cards-2');
+  const triggerBottom = window.innerHeight * 0.85;
 
-new FinisherHeader(
-    {
-  count: 12,
-  size: {
-    min: 1300,
-    max: 1500,
-    pulse: 0
-  },
-  speed: {
-    x: {
-      min: 0.6,
-      max: 3
-    },
-    y: {
-      min: 0.6,
-      max: 3
+  cards.forEach(card => {
+    const cardTop = card.getBoundingClientRect().top;
+    if (cardTop < triggerBottom) {
+      card.classList.add('visible');
+    } else {
+      card.classList.remove('visible');
     }
-  },
-  colors: {
-    background: "#953eff",
-    particles: [
-      "#ff681c"
-    ]
-  },
-  blending: "lighten",
-  opacity: {
-    center: 0.6,
-    edge: 0
-  },
-  skew: -2,
-  shapes: [
-    "c"
-  ]
-})
+  });
+};
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
+// ____________________________________________________________________________________________
+
+function showOnScroll() {
+  const project = document.querySelectorAll('.single-project1, .single-project2, .single-project3');
+  const triggerBottoms = window.innerHeight * 0.85;
+
+  project.forEach(cards => {
+    
+    const cardTops = cards.getBoundingClientRect().top;
+    
+    if (cardTops < triggerBottoms) {
+      cards.classList.add('visible');
+
+    } else {
+      cards.classList.remove('visible');
+    }
+  });
+};
+
+window.addEventListener('scroll', showOnScroll);
+window.addEventListener('load', showOnScroll);
+// see more and hideup and down text
+
+// const text = document.getElementById("blurText");
+// const button =document.querySelectorAll(".btn");
+// const buttonEach1 =document.querySelector(".seeMore")
+// const buttonEach2 =document.querySelector(".hideLess")
+
+// button.forEach(function(btns)
+// {
+//   console.log(btns);
+//   btns.addEventListener("click" ,(event)=>
+//   {
+//     let btn =event.currentTarget.classList;
+    
+//     if(btn.contains("seeMore"))
+//     {
+//       text.classList.remove("hiddinText");
+//   console.log(text);
+//   buttonEach1.style.display = "none";
+//   buttonEach2.style.display ="inline";
+//     }
+//     else if(btn.contains("hideLess"))
+//     {
+//       text.classList.add("hiddinText");
+//   console.log(text);
+//   buttonEach2.style.display = "none";
+//   buttonEach1.style.display ="inline";
+//     }
+//   })
+// })
+// const buttonEach3= document.querySelector(".seeMore1")
+// const buttonEach4 = document.querySelector(".hideLess1")
+// const button1 =document.querySelectorAll(".btn1");
+// const text1 = document.getElementById("shayan1")
+// console.log(text1)
+// button1.forEach(function(btns)
+// {
+//   console.log(btns);
+//   btns.addEventListener("click" ,(event)=>
+//   {
+//     let button1 =event.currentTarget.classList;
+    
+//     if(button1.contains("seeMore1"))
+//     {
+//       text1.classList.remove("sheikh1");
+//   console.log(text);
+//   buttonEach3.style.display = "none";
+//   buttonEach4.style.display ="inline";
+//     }
+//     else if(button1.contains("hideLess1"))
+//     {
+//       text1.classList.add("sheikh1");
+//   console.log(text);
+//   buttonEach4.style.display = "none";
+//   buttonEach3.style.display ="inline";
+//     }
+//   })
+// })
