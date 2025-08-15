96ef0e258c79abfafac28dd66c3164d04412d5cb
ShayanSheikh1936 <sheikhshayan082@gmail.com>
2025-08-15 21:44:06 +0500

    Our portfolio

--- Dynamics/script.js
+++ Dynamics/script.js
@@ -92,46 +92,84 @@ window.addEventListener('load', showOnScroll);
 //     }
 //   })
 // })
-const button = document.querySelectorAll(".btn")
-const hideBtn = document.querySelector(".btn1")
-
+const hideBtn = document.querySelector(".hide-tap")
+const seeBtn = document.querySelector(".seemore-tap")
+const button = document.querySelectorAll("#btn")
+ console.log(button);
+ 
 button.forEach(function(buttons)
 {
   buttons.addEventListener("click", ()=>
-    {
-      buttons.style.display ="none";
-      console.log(button);
-    })
-    hideBtn.addEventListener("click", (buttons)=>
-      {
-        buttons.style.display = "inline";
-      })
-})
-
-
-  hideBtn.addEventListener("click", (button)=>
   {
-    button.preventDefault();
-    
-    button.style.display = "inline";
-    
-  })
-
-// eduacation collapse cards
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
+    if(buttons.classList.contains("seemore-tap"))
+      {
+        console.log(buttons);
+        
+        seeBtn.style.display = "none";
+        hideBtn.style.display ="inline";
+      }
+      else if (buttons.classList.contains("hide-tap"))
+      {
+        hideBtn.style.display ="none";
+        seeBtn.style.display ="inline";
+      }
   });
-}
+  const hideBtn1 = document.querySelector(".hide-tap1")
+const seeBtn1= document.querySelector(".seemore-tap1")
+const button1 = document.querySelectorAll("#btn1")
+ console.log(button);
+ 
+button1.forEach(function(buttons1)
+{
+  buttons1.addEventListener("click", ()=>
+  {
+    if(buttons1.classList.contains("seemore-tap1"))
+      {
+        console.log(buttons);
+        
+        seeBtn1.style.display = "none";
+        hideBtn1.style.display ="inline";
+      }
+      else if (buttons1.classList.contains("hide-tap1"))
+      {
+        hideBtn1.style.display ="none";
+        seeBtn1.style.display ="inline";
+      }
+  });
+//  -------------------------------- 
+const hideBtn2 = document.querySelector(".hide-tap2")
+const seeBtn2 = document.querySelector(".seemore-tap2")
+const button2 = document.querySelectorAll("#btn2")
+ 
+button2.forEach(function(buttons2)
+{
+  buttons2.addEventListener("click", ()=>
+  {
+    if(buttons2.classList.contains("seemore-tap2"))
+      {
+        console.log(buttons);
+        
+        seeBtn2.style.display = "none";
+        hideBtn2.style.display ="inline";
+      }
+      else if (buttons2.classList.contains("hide-tap2"))
+      {
+        hideBtn2.style.display ="none";
+        seeBtn2.style.display ="inline";
+      }
+  });
+
+  // buttons.addEventListener("click", ()=>
+  //   {
+  //     buttons.style.display ="none";
+  //     console.log(button);
+  //   })
+  //   hideBtn.addEventListener("click", (buttons)=>
+  //     {
+  //       buttons.style.display = "inline";
+  //     })
+})
+// ----------------------------------------------------------
 
 function showEducationOnScroll() {
   const educationCards = document.querySelectorAll('.education-mobile-width');
@@ -151,17 +189,44 @@ window.addEventListener('scroll', showEducationOnScroll);
 window.addEventListener('load', showEducationOnScroll);
 // -------------------------------------------------------------------
 
-const loading =document.getElementById("loading-spinner")
-const overallPage =document.querySelector(".overall-page")
+// const loading =document.getElementById("loading-spinner")
+// const overallPage =document.querySelector(".overall-page")
 
-setTimeout(() => {
-  loading.style.display = "none";
-  document.body.style.height = "100px";
-  if(loading.style.display ==="none")
-  {
-    overallPage.style.display= "block"
-  }
-}, 2000);
+// setTimeout(() => {
+//   loading.style.display = "none";
+//   document.body.style.height = "100px";
+//   if(loading.style.display ==="none")
+//   {
+//     overallPage.style.display= "block"
+//   }
+// }, 2000);
 
 const skills = document.querySelector(".logo-slider")
 
+const showcaseBtn = document.querySelectorAll("#showcase-buttons");
+const Certificates_btn = document.querySelector(".certificate")
+const project_btn = document.querySelector(".projects-section")
+const learnmore_btn =document.querySelector(".learn-more")
+const seemore_btn =document.querySelector(".seemore-btn")
+showcaseBtn.forEach((tap) => {
+  tap.addEventListener("click", () => {
+    if (tap.classList.contains("project-btn")) {
+      console.log("correct");
+      project_btn.style.display = "block";
+      Certificates_btn.style.display= "none";
+    } else if (tap.classList.contains("Certificates-btn")) {
+      console.log("good");
+      project_btn.style.display = "none";
+      Certificates_btn.style.display= "block";
+      skills.style.transition = "10s ease"
+    } else if (tap.classList.contains("hobbies-btn")) {
+      console.log("excellent");
+      project_btn.style.display = "none";
+      Certificates_btn.style.display= "none";
+      learnmore_btn.style.display = "none";
+      seemore_btn.style.padding = 0;
+    } 
+  });
+});
+});
+});
